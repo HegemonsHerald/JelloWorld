2aac26d2612267ef661e089e4eb704396161ae9f
RevMatres <benediktheinen1@gmail.com>
2018-11-11 15:18:00 +0100

    finishing that extra task from series 2

--- src/series2/Starlygon.java
+++ src/series2/Starlygon.java
@@ -8,76 +8,127 @@ import acm.graphics.*;
  */
 public class Starlygon extends GraphicsProgram {
 
-	public void drawStarlygon() {
+	// 'Star' is boring.
+	// I will call it Starlygon. That's objectively better.
 
-		// 
-		int n = readInt("number? ");
-		int innerRadius = readInt("inner? ");
-		int outerRadius = readInt("outer? ");
-		boolean largeR = false;
+	@Override
+	public void run() {
 
 		// Resize factor to make that damn thing visible
 		int resizeFactor = 10;
 
-		// If you want to draw lines from duck to duck, you need to remember, where the last duck was!
-		// This is initialized with the position of the first duck of the duck-lygon
-		GPoint lastDuck = new GPoint(1 * outerRadius + outerRadius, 0 * outerRadius + outerRadius);
-		/* Needs more explanation
+		// hmmmmmm....
+		println("So you want to make a Star-lygon....");
+
+		// Get Input
+		int n = readInt("Well, then. How many pointy bits? ");
+		int innerRadius = readInt("And an inner radius, please? ");
+		int outerRadius = readInt("How about an outer radius? ");
+
+		if (innerRadius > outerRadius) {
+			println("You do know the Starlygon won't be completely visible, if you enter an outer Radius that's larger than the inner Radius, right?");
+			println("Just asking to be sure, I mean, I take it you know what you're doing... Cause whey the ████ else would you be playing with computers, right? Right? ... RIGHT 😅 ?");
+		}
+
+
+		// Now for making the thing
+
+		/* Bit of an explanation
+		 *
+		 * The Starlygon is drawn using a loop. On each iteration a new point is computed.
+		 *
+		 * That new point is either innerRadius or outerRadius away from the Starlygon's center,
+		 * which is determined by the largeR flag variable. If the last point had outerRadius,
+		 * the new point needs to have innerRadius and vice versa, cause the star goes zick-zack.
+		 *
+		 * A line is then drawn from the last point to the newly made point. To make that possible
+		 * I use lastPoint to store the last-ly computed point. This means I get around recomputing
+		 * points I already computed, which is always good. It also means I have to initialize
+		 * lastPoint with the position of the first point of the Starlygon and start the loop with
+		 * computing the second point of the Starlygon.
+		 *
+		 * The loop goes round 2*n-1 times, in accordance with the task's description and the use
+		 * of lastPoint.
 		 *
 		 */
 
-		// Loop yay, note that the loop starts with the second duck cause we already know where the first duck goes!
+		// Flag variable to tell you wether to use the large outer radius,
+		// or the smaller inner radius
+		boolean largeR = false; // largeR as in large Radius
+
+		// Variable to hold the last point, of the drawing
+		// Initialized with the first point, so the the GLine can be drawn in the loop
+		GPoint lastPoint = new GPoint(1 * outerRadius + outerRadius, 0 * outerRadius + outerRadius);
+		// note: this is explained in detail below, where the point-making process is outlined in detail...
+
+		// Loop yay, note that the loop starts with the second point cause we already know where the first point goes!
+		// 2n cause we have 2n points to draw
 		for (int i = 1; i <= 2*n; i++) {
 
-			// How far have you gotten round the duck-lygon?
-			double currAngle = i * (Math.PI / n); // same as ((2*Math.PI / n) / 2); cause we have 2n points to draw!
-
+			// Radius will be used to scale the point from unit circle to Starlygon
 			int radius = 0;
+
+			// Check the state of largeR
 			if (largeR) {
+
+				// largeR == true? Use the larger radius!
 				radius = outerRadius;
+
+				// The next point should use the innerRadius
 				largeR = false;
+
 			} else if (!largeR) {
+
+				// largeR == false? Use the smaller radius!
 				radius = innerRadius;
+
+
+				// The next point should use the outerRadius!
 				largeR = true;
 			}
 
-			// Calculate where the current duck is
-			//
-			// - sin and cos produce the point on the unit circle,
-			//   that corresponds to currAngle
-			// - * radius scales the vector to that point from length 1
-			//   to either the innerRadius or the outerRadius
-			// - + outerRadius moves the point into the visible space,
-			//   because the acm can't handle negative coordinates
+
+			// Now let's make a point!
+
+			// How far have you gotten round the Starlygon?
+			double currAngle = i * (Math.PI / n); // same as (2*Math.PI / 2n) 
+
+			// Calculate a new point... the current point.
 			double x = Math.cos(currAngle) * radius + outerRadius;
 			double y = Math.sin(currAngle) * radius + outerRadius;
 
-			GPoint currDuck = new GPoint(x, y);
+			GPoint currPoint = new GPoint(x, y);
 
-			// Draw the line from the last duck to the current duck
+			/*
+			 * First calculate the point's position on the unit circle using cos() or sin() for x and y
+			 * respectively.
+			 *
+			 * The unit circle position is then scaled with radius, to make the point either be innerRadius
+			 * or outerRadius away from the Starlygon's center.
+			 *
+			 * In the end they get outerRadius added onto them, as an offset to make sure all Starlygon
+			 * points are in the positive space of the coordinate grid (cause acm don't do negative numbers).
+			 * I use outerRadius cause it's the smallest number that would move the Starlygon into visible space,
+			 * since a point can maximally be outerRadius far away of the Starlygon's center.
+		 	 *
+			 * The initial point is computed the same way: since largeR is set to false and takes
+			 * effect only on the first point the loop computes, the intial point, which comes before that,
+			 * needs to be computed with a largeR set to true.
+			 * I then simply declare that the Starlygon starts at x=1 and y=0, which is plugged into
+			 * the formula described above to create the coordinates.
+			 */
+
+			// Draw the line from the last point to the current duck
 			add(new GLine(
-					lastDuck.getX() * resizeFactor,
-					lastDuck.getY() * resizeFactor,
-					currDuck.getX() * resizeFactor,
-					currDuck.getY() * resizeFactor
+					lastPoint.getX() * resizeFactor, // Scale the crap to make it visible, eh!
+					lastPoint.getY() * resizeFactor,
+					currPoint.getX() * resizeFactor,
+					currPoint.getY() * resizeFactor
 			));
 
-			println(largeR);
-
-			// Make the last duck be the current duck
-			lastDuck = currDuck;
+			// Make the last point be the current point
+			lastPoint = currPoint;
 		}
+
 	}
-
-	public void run() {
-		add(new GLabel("Hoho, I hate both Java and the ACM, and the latter even more than the former!"), 190, 100);
-
-		// you also still need to check, that the rotation is as they want it from the task...
-
-		println("So you want to make a Duck-lygon....");
-
-		// make a duck-lygon
-		drawStarlygon();
-	}
-
 }
