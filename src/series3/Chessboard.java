6c66f707e16ea567cdf65a2468d492c7a0760f39
work <work@admins-MacBook-Pro.local>
2018-11-14 13:48:29 +0100

    starting to implement the chessboard

---
+++ src/series3/Chessboard.java
@@ -0,0 +1,16 @@
+package series3;
+
+import acm.program.GraphicsProgram;
+import acm.graphics.*;
+
+/**
+ * I hope you realise you told us in class how to solve this problem...?
+ */
+public class Chessboard extends GraphicsProgram {
+
+	@Override
+	public void run() {
+
+	}
+
+}
