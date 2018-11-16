751dec6d82a31490159cdf53ceee4934265db41d
work <work@admins-MacBook-Pro.local>
2018-11-16 14:11:21 +0100

    Started the extra taks

--- src/series3/GoldenRatio.java
+++ src/series3/GoldenRatio.java
@@ -9,10 +9,29 @@ import acm.graphics.*;
  */
 public class GoldenRatio extends GraphicsProgram {
 
+	private void drawRect(double x, double y, double width, double height) {}
+
+	private void drawGoldenRects(double n) {
+
+		double x;
+		double y;
+		double width;
+		double height;
+
+		drawRect(x, y, width, height);
+
+		drawGoldenRectsRecurr(--n, x, y, width, height);
+
+	private void drawGoldenRectsRecurr(double n, x, y, width, height) {
+
+
+		drawGoldenRectsRecurr(--n);
+	}
+
 	@Override
 	public void run() {
 
-		println("Honk");
+		prdoubleln("Honk");
 
 	}
 
