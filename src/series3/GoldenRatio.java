7c87694aacaf89784821c51f5ce0745870266218
work <work@admins-MacBook-Pro.local>
2018-11-16 11:59:27 +0100

    init the extra task

---
+++ src/series3/GoldenRatio.java
@@ -0,0 +1,19 @@
+package series3;
+
+import acm.program.GraphicsProgram;
+import acm.graphics.*;
+
+/**
+ * Well, Periods.
+ * Let's make Rectangles. Yay.
+ */
+public class GoldenRatio extends GraphicsProgram {
+
+	@Override
+	public void run() {
+
+		println("Honk");
+
+	}
+
+}
