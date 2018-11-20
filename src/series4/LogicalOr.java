3ce03b16a2e2cb3e8a0f6da5c746cc31dd425d05
work <work@admins-MacBook-Pro.local>
2018-11-20 13:24:54 +0100

    started series 4 of programming

---
+++ src/series4/LogicalOr.java
@@ -0,0 +1,19 @@
+package series4;
+
+import acm.program.*;
+import acm.graphics.*;
+
+class LogicalOr extends ConsoleProgram {
+
+	public void run() {
+
+		// The following DOES NOT FUCKING WORK
+
+		switch (32) {
+//			case (27 || 19): println("Honk"); break;
+//			case (42 || 32): println("Tonk"); break;
+		}
+
+	}
+
+}
---
+++ src/series4/MethodicalPyramid.java
@@ -0,0 +1,14 @@
+package series3;
+
+import acm.program.*;
+import acm.graphics.*;
+
+class MethodicalPyramid extends GraphicsProgram {
+
+	public void run() {
+
+		println("Honk");
+
+	}
+
+}
