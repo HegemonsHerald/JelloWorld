2eb0e03b2260854be3b36f3ec120a58be90a224b
work <work@admins-MacBook-Pro.local>
2018-11-27 14:52:15 +0100

    tried division by zero with floats... it was interesting

---
+++ src/series5/honk.java
@@ -0,0 +1,19 @@
+package series5;
+
+/**
+ * NAME.
+ */
+public class honk {
+
+	public static void main(String[] args) {
+
+
+		// Ok, so this yields NaN, but you really shouldn't use that!
+		float honk = (float) 0 / (float) 0;
+		System.out.println(honk);
+		System.out.println(42 * honk);
+
+
+	}
+
+}
