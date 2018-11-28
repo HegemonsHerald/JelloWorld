24fac4ee4ef0284455852be8ae1cc0dec508e7bb
work <work@admins-MacBook-Pro.local>
2018-11-28 11:08:42 +0100

    one-liner javadoc and local/enclosed classes

--- src/series5/honk.java
+++ src/series5/honk.java
@@ -5,14 +5,21 @@ package series5;
  */
 public class honk {
 
+	/** is this javadoc? */
+	public class TonyHawk {}
+
 	public static void main(String[] args) {
 
 
 		// Ok, so this yields NaN, but you really shouldn't use that!
-		float honk = (float) 0 / (float) 0;
-		System.out.println(honk);
-		System.out.println(42 * honk);
+		float flonk = (float) 0 / (float) 0;
+		System.out.println(flonk);
+		System.out.println(42 * flonk);
 
+
+		// Example of a local class
+		honk tonk = new honk(); // you need an instance the enclosing class of TonyHawk...
+		TonyHawk timmy = tonk.new TonyHawk(); // ... to make an instance of TonyHawk
 
 	}
 
