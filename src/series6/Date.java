6ec304981025c00e673be0d955a47d594ef7dc44
work <work@admins-MacBook-Pro.local>
2018-12-03 17:24:06 +0100

    started series 6

---
+++ src/series6/Date.java
@@ -0,0 +1,12 @@
+package series6;
+
+//package programming.set6.date;
+
+/**
+ * Date. Yup.
+ */
+public class Date {
+
+	static final String months[] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+}
