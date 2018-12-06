6aabfdbec9c2ebf49c010bb7096c6fb149976273
work <work@admins-MacBook-Pro.local>
2018-12-06 20:09:03 +0100

    added testing and some of the functions for the date task

--- src/series6/Date.java
+++ src/series6/Date.java
@@ -10,15 +10,38 @@ public class Date {
 	/** The Months of the Gregorian Calendar as Strings. */
 	private static final String MONTHS[] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
 
+	// The required fields to represent a date
 	private int year;
 	private int month;
 	private int day;
+
+	/** getYear.
+	 * @return the year
+	 */
+	public int getYear() {
+		return year;
+	}
+
+	/** getMonth.
+	 * @return the month
+	 */
+	public int getMonth() {
+		return month;
+	}
+
+	/** getDay.
+	 * @return the day
+	 */
+	public int getDay() {
+		return day;
+	}
 
 	/** Is a year a leap year?
 	 * @param year the year to test
 	 * @return true if the year is a leap year
 	 */
-	private static boolean isLeapYear(int year) {
+	//private static boolean isLeapYear(int year) {
+	public static boolean isLeapYear(int year) {
 
 		/* A year is a leap year if it is divisible by 4
 		 * and not divisible by 100, unless it is divisible
@@ -36,6 +59,16 @@ public class Date {
 	 * @return true if the year is valid
 	 */
 	public static boolean validate(int year, int month, int day) {
+
+		// Either the month is out of the range of a year...
+		if (month < 1 || month > 12) return false;
+
+		// ... or the day is out of the range of possible days in that month...
+		else if (day < 1 || day > getDaysInMonth(year, month)) return false;
+
+		// ... or it's valid
+		else return true;
+
 	}
 
 	/** Construct a Date Instance.
@@ -45,9 +78,71 @@ public class Date {
 	 * @return a Date.
 	 */
 	public Date(int year, int month, int day) {
+
+		// Check validity
+		if (!validate(year, month, day)) {
+			throw new IllegalArgumentException("This is not a valid date.");
+		}
+
+		// Make Date
+		this.year  = year;
+		this.month = month;
+		this.day   = day;
+
 	}
 
+	/** Gets how many days there are in a month in a year.
+	 * @param year year.
+	 * @param month month.
+	 * @return whatcha asked.
+	 */
 	public static int getDaysInMonth(int year, int month) {
+
+		// Wenn Monat kleiner-gleich 7 und ungerade ist 31 Tage,
+		// wenn grőßer als 7 und gerade ist 31 Tage
+		// If the month is february...
+		if (month == 2) {
+
+			if (isLeapYear(year)) return 29;
+			else return 28;
+
+		// ... if month between January and July...
+		} else if (month <= 7) {
+
+			// ... return 30 if even and 31 if odd...
+			if (month%2==0) return 30;
+			else return 31;
+
+		// ... if month between August and December...
+		} else if (month > 7) {
+
+			// ... return 31 if even and 30 if odd
+			if (month%2==0) return 31;
+			else return 30;
+
+		} else return 0;
+
+	}
+
+	/** Returns the absolute day count of the date, in reference to 365/366 days in a whole year.
+	 * @return
+	 */
+	public int dayOfYear() {
+		return 0;
+	}
+
+	/** Returns the difference between two dates, provided both are in the same year.
+	 * If the dates aren't in the same year the method returns 0.
+	 * @param other
+	 * @return
+	 */
+	public int sameYearDiff(Date other) {
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return MONTHS[this.month + 1] + " " + this.day + ", " + this.year;
 	}
 
 }
---
+++ src/series6/DateTestground.java
@@ -0,0 +1,60 @@
+package series6;
+
+import series6.*;
+
+/**
+ * NAME.
+ */
+public class DateTestground {
+
+	public static void main(String args[]) {
+
+		// create a date
+		Date d1 = new Date(2016, 3, 26);
+		Date d2 = new Date(2016, 12, 15);
+		Date d3 = new Date(2016, 1, 24);
+		Date d4 = new Date(12, 1, 24);
+
+		// check toString
+		System.out.println("check println");
+		System.out.println(d1);
+		System.out.println();
+
+		// check for leap years
+		System.out.println("is leap year with 2016 and 2017");
+		System.out.println(Date.isLeapYear(2016));
+		System.out.println(Date.isLeapYear(2017));
+		System.out.println();
+
+		// check for days in month
+		System.out.println("how many days in month with february 2016, feb 2017, march, january, december, november");
+		System.out.println(Date.getDaysInMonth(2016, 2));
+		System.out.println(Date.getDaysInMonth(2017, 2));
+		System.out.println(Date.getDaysInMonth(2016, 3));
+		System.out.println(Date.getDaysInMonth(2016, 1));
+		System.out.println(Date.getDaysInMonth(2016, 12));
+		System.out.println(Date.getDaysInMonth(2016, 11));
+		System.out.println();
+
+		// check validation
+		System.out.println("date validation");
+		System.out.println(Date.validate(2017, 2, 33));
+		System.out.println(Date.validate(2018, 12, 17));
+		System.out.println();
+
+		// check dayOfYear
+		System.out.println("day of year");
+		System.out.println(d1.dayOfYear());
+		System.out.println();
+
+		// check sameYearDiff
+		System.out.println("sameYearDiff first with self, future, past, outOfBounds");
+		System.out.println(d1.sameYearDiff(d1));
+		System.out.println(d1.sameYearDiff(d2));
+		System.out.println(d1.sameYearDiff(d3));
+		System.out.println(d1.sameYearDiff(d4));
+		System.out.println();
+
+	}
+
+}
