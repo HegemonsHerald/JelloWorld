e7e7f2279192a168b0d177d2f69e9ae793abbafb
RevMatres <benediktheinen1@gmail.com>
2018-12-09 17:01:45 +0100

    made the eratosthenes program at uni without my computer, so this is a bit of a mess, with java's own output

---
+++ src/series6/SieveOfEratosthenes.java
@@ -0,0 +1,80 @@
+// import acm.program.ConsoleProgram;
+
+/**
+ * Was is Sieben mal Sieben? Feiner Sand.
+ */
+// public class SieveOfEratosthenes extends ConsoleProgram {
+public class SieveOfEratosthenes {
+
+	/** Checks whether a number is a prime or not
+	 * @param a a number to check for primety
+	 * @return whether it's a prime or not
+	 */
+	private static boolean isPrime(int a) {
+		for (int i = 2; i<a; i++) {
+			if (a%i==0) return false;
+		}
+		return true;
+	}
+
+	// @Override
+	// public void run() {
+	public static void main(String args[]) {
+
+		/* Get Input */
+
+		System.out.println("Give me an Integer that's larger or at least equal to 2: ");
+		int n = 0;
+
+		/*
+		   do {
+		   n = readInt("Give me an Integer that's larger or at least equal to 2: ");
+		   } while (n < 2);
+		   */
+
+		n = 20;
+
+
+		/* Init Computation Array */
+
+		// Init numbers array
+		int numbers[] = new int[n-1];
+
+		// Fill array with appropriate values
+		int count = 2;
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = count++;
+		}
+
+
+		/* Execute Eratosthenes' algorithm */
+
+		// Loop!
+		for (int i = 0; i < numbers.length; i++) {
+
+			int a = numbers[i];
+
+			// If the number is set to 0, there's nothing to do...
+			if (a == 0) continue;
+
+			// ... else if the number is prime
+			else if (isPrime(a)) {
+
+				// ... output it...
+				System.out.println(a);
+
+				// ... set all multiples of the number to 0...
+				for (int j = 1; j*a < numbers.length; j++) {
+					numbers[(j*a) -2] = 0;
+				}
+
+				// ... else there's nothing to do
+			} else continue;
+
+
+		}
+
+
+
+	}
+	}
